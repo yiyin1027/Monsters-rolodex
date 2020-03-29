c875a43aeb389590092a70ff5ffc69519c1e2bdb
--replace-all <yzhan647@uottawa.ca>
2020-03-29 17:23:33 -0400

    Updates

---
+++ precache-manifest.5f12fa9704d6b61abac458bb389a288c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7dcf9548938ae79f56126365942b6ed3",
+    "url": "/Monsters-rolodex/index.html"
+  },
+  {
+    "revision": "dbb6c9e24c1df62a2f75",
+    "url": "/Monsters-rolodex/static/css/main.5deb32f0.chunk.css"
+  },
+  {
+    "revision": "6662efd7ddb56209bad9",
+    "url": "/Monsters-rolodex/static/js/2.7e85b794.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/Monsters-rolodex/static/js/2.7e85b794.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "dbb6c9e24c1df62a2f75",
+    "url": "/Monsters-rolodex/static/js/main.b57e33ea.chunk.js"
+  },
+  {
+    "revision": "30101cb0edfcabf7dc8e",
+    "url": "/Monsters-rolodex/static/js/runtime-main.1afd945f.js"
+  }
+]);
